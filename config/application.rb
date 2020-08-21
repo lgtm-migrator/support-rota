c9b41705975b652b95b300a095533a83b470ccba
Stuart Harrison <pezholio@gmail.com>
2020-08-21 09:35:22 +0100

    Remove database config

--- config/application.rb
+++ config/application.rb
@@ -2,11 +2,20 @@
 
 require_relative "boot"
 
-require "rails/all"
+require "active_model/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "active_job/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+require "./lib/patterdale"
 
 # TODO: Name the application
 module RailsTemplate
