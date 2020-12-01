ffd047a54c10803bbaf97842d8a2303708ac43d9
Ed Davey <ed@veryreal.co.uk>
2020-12-01 15:07:10 +0000

    Make HTTP stubbing more reliable

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,3 +4,7 @@ require "webmock/cucumber"
 
 WebMock.disable_net_connect!(allow_localhost: true)
 Capybara.default_host = "http://localhost:3000"
+
+Before do
+  stub_opsgenie_schedules
+end
--- features/support/opsgenie_stubs.rb
+++ features/support/opsgenie_stubs.rb
@@ -1,21 +1,23 @@
-WebMock.stub_request(:get, %r(schedules/e71d500f-896a-4b28-8b08-3bfe56e1ed76))
-  .to_return(
-    status: 200,
-    body: File.read(
-      Rails.root.join(
-        'spec', 'fixtures', 'schedule_e71d500f_with_rotations.json'
-      )
-    ),
-    headers: {"Content-Type" => "application/json"}
-  )
+def stub_opsgenie_schedules
+  WebMock.stub_request(:get, %r{schedules/e71d500f-896a-4b28-8b08-3bfe56e1ed76})
+    .to_return(
+      status: 200,
+      body: File.read(
+        Rails.root.join(
+          "spec", "fixtures", "schedule_e71d500f_with_rotations.json"
+        )
+      ),
+      headers: {"Content-Type" => "application/json"}
+    )
 
-WebMock.stub_request(:get, %r(schedules/b8e97704-0e9d-41b5-b27c-9d9027c83943))
-  .to_return(
-    status: 200,
-    body: File.read(
-      Rails.root.join(
-        'spec', 'fixtures', 'schedule_b8e97704_with_rotations.json'
-      )
-    ),
-    headers: {"Content-Type" => "application/json"}
-  )
+  WebMock.stub_request(:get, %r{schedules/b8e97704-0e9d-41b5-b27c-9d9027c83943})
+    .to_return(
+      status: 200,
+      body: File.read(
+        Rails.root.join(
+          "spec", "fixtures", "schedule_b8e97704_with_rotations.json"
+        )
+      ),
+      headers: {"Content-Type" => "application/json"}
+    )
+end
